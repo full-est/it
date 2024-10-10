d04a2fe6233eec761297a849af2beef66c2884f6
full-est <xamilion48@gmail.com>
2024-10-11 01:20:26 +0500

    lab3

--- src/lab3/Main2.java
+++ src/lab3/Main2.java
@@ -2,34 +2,26 @@ package lab3;
 
 public class Main2 {
     public static void main(String[] args) {
-        // Создаем экземпляр хэш-таблицы
         HashTable<String, Integer> hashTable = new HashTable<>();
 
-        // Добавляем несколько пар ключ-значение
         hashTable.put("Apple", 50);
         hashTable.put("Banana", 30);
         hashTable.put("Orange", 20);
         hashTable.put("Grapes", 40);
 
-        // Выводим текущее содержимое хэш-таблицы
         System.out.println("Хэш-таблица после добавления элементов:");
         hashTable.printTable();
 
-        // Получаем значение по ключу
         System.out.println("\nЗначение для ключа 'Banana': " + hashTable.get("Banana"));
 
-        // Удаляем пару по ключу
         hashTable.remove("Orange");
         System.out.println("\nХэш-таблица после удаления 'Orange':");
         hashTable.printTable();
 
-        // Проверяем размер таблицы
         System.out.println("\nТекущий размер хэш-таблицы: " + hashTable.size());
 
-        // Проверяем, пуста ли таблица
         System.out.println("\nХэш-таблица пуста? " + hashTable.isEmpty());
 
-        // Пытаемся получить значение для удаленного ключа
         System.out.println("\nЗначение для ключа 'Orange' (после удаления): " + hashTable.get("Orange"));
     }
 }
